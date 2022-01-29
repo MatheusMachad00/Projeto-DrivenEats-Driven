5817490c244892491c5078b814222b6abae81509
MatheusMachad00 <matheus_m_machado@hotmail.com>
2022-01-29 17:19:01 -0300

    Implementação da borda ao selecionar o item desejado

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
-function removerBorda(){
-    let bordas = document.getElementById("bife");
-    bordas.classList.toggle("borda");
+let prato = null;
+let bebidaEscolhida = null;
+let sobremesaEscolhida = null;
+
+function removerBorda(classeItem, nomeExibicao){
+    const selecionado = document.querySelector(".comida .borda");
+    if (selecionado !== null){
+        selecionado.classList.remove("borda");
+    }
+    const botao = document.querySelector("."+classeItem);
+    botao.classList.add("borda");
+    prato = nomeExibicao;
+    /*if (nomeExibicao !== null){
+        document.getElementById("check").style.display = "block";
+    }else{
+        document.getElementById("check").style.display = "none";
+    }
     
+    document.getElementsByTagName("ion-icon").style.display = "block"; */
 }
+function removerBorda2(classeItem, nomeExibicao){
+    const selecionado = document.querySelector(".bebida .borda");
+    if (selecionado !== null){
+        selecionado.classList.remove("borda");
+    }
+    const botao = document.querySelector("."+classeItem);
+    botao.classList.add("borda");
+    bebidaEscolhida = nomeExibicao;
+}
+function removerBorda3(classeItem, nomeExibicao){
+    const selecionado = document.querySelector(".sobremesa .borda");
+    if (selecionado !== null){
+        selecionado.classList.remove("borda");
+    }
+    const botao = document.querySelector("."+classeItem);
+    botao.classList.add("borda");
+    sobremesaEscolhida = nomeExibicao;
+}
+
 
