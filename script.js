5b633c6dffdbf087af431e7360a7ae504b1fd47e
MatheusMachad00 <matheus_m_machado@hotmail.com>
2022-01-30 17:19:49 -0300

    implementação botão do footer

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 let prato = null;
 let bebidaEscolhida = null;
 let sobremesaEscolhida = null;
+let precoPrato = 0;
+let precoBebida = 0;
+let precoSobremesa = 0;
+let total = 0; //problemas com o total.
 
-function removerBorda(classeItem, nomeExibicao){
+function ConfirmarPedido(){
+    if(prato !== 0 && bebidaEscolhida !== 0 && sobremesaEscolhida !== 0){
+        const botao = document.querySelector('.pedidoFinalizado');
+        botao.classList.toggle('pedidoFinalizado');
+        botao.innerHTML = 'Fechar pedido';
+    }
+}
+function removerBorda(classeItem, nomeExibicao, precoItem){
     const selecionado = document.querySelector(".comida .borda");
     if (selecionado !== null){
         selecionado.classList.remove("borda");
@@ -10,15 +21,10 @@ function removerBorda(classeItem, nomeExibicao){
     const botao = document.querySelector("."+classeItem);
     botao.classList.add("borda");
     prato = nomeExibicao;
-    /*if (nomeExibicao !== null){
-        document.getElementById("check").style.display = "block";
-    }else{
-        document.getElementById("check").style.display = "none";
-    }
-    
-    document.getElementsByTagName("ion-icon").style.display = "block"; */
+    precoPrato = precoItem;
 }
-function removerBorda2(classeItem, nomeExibicao){
+function removerBorda2(classeItem, nomeExibicao, precoItem2){
+    precoItem = precoBebida;
     const selecionado = document.querySelector(".bebida .borda");
     if (selecionado !== null){
         selecionado.classList.remove("borda");
@@ -26,8 +32,10 @@ function removerBorda2(classeItem, nomeExibicao){
     const botao = document.querySelector("."+classeItem);
     botao.classList.add("borda");
     bebidaEscolhida = nomeExibicao;
+    precoBebida = precoItem2;
 }
-function removerBorda3(classeItem, nomeExibicao){
+function removerBorda3(classeItem, nomeExibicao, precoItem3){
+    precoItem = precoSobremesa;
     const selecionado = document.querySelector(".sobremesa .borda");
     if (selecionado !== null){
         selecionado.classList.remove("borda");
@@ -35,6 +43,14 @@ function removerBorda3(classeItem, nomeExibicao){
     const botao = document.querySelector("."+classeItem);
     botao.classList.add("borda");
     sobremesaEscolhida = nomeExibicao;
+    precoSobremesa = precoItem3;
+    ConfirmarPedido();
 }
 
-
+function finalizarPedido(){
+    ConfirmarPedido();
+    total = parseFloat(precoPrato + precoBebida + precoSobremesa);
+    let mensagem = null;
+    mensagem = 'Olá, gostaria de fazer o pedido:\n- Prato: ${prato}\n- Bebida: ${bebidaEscolhida}\n- Sobremesa: ${sobremesaEscolhida}\nTotal: R$ ${total}';
+    window.open(`https://wa.me/9999999999999?text=${encodeURIComponent(mensagem)}`)
+}
