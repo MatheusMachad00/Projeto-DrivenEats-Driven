638a528a9a3f1a65bfa7b8b5282a94011ce810dd
MatheusMachad00 <matheus_m_machado@hotmail.com>
2022-01-31 15:24:06 -0300

    ajustes finais

--- script.js
+++ script.js
@@ -4,18 +4,28 @@ let sobremesaEscolhida = null;
 let precoPrato = 0;
 let precoBebida = 0;
 let precoSobremesa = 0;
-let total = (precoPrato + precoBebida + precoSobremesa);
+let total = 0;
 
 function FecharPedido() {
     const botao1 = document.querySelector('.pedidoFinalizado');
     if (botao1 !== null) {
-        if (prato !== 0 && bebidaEscolhida !== 0 && sobremesaEscolhida !== 0) {
+        if (prato !== null && bebidaEscolhida !== null && sobremesaEscolhida !== null) {
             const botao = document.querySelector('.pedidoFinalizado');
             botao.classList.toggle('pedidoFinalizado');
             botao.innerHTML = 'Fechar pedido';
         }
     }
 }
+/*function removerCheck(){
+    const icone = document.querySelector(".box_check .check");
+    if (icone !== null){
+        icone.classList.remove("check");
+    } else {
+        icone.classList.add("check");
+    }
+
+} */
+
 function removerBorda(classeItem, nomeExibicao, precoItem) {
     const selecionado = document.querySelector(".comida .borda");
     if (selecionado !== null) {
@@ -25,7 +35,8 @@ function removerBorda(classeItem, nomeExibicao, precoItem) {
     botao.classList.add("borda");
     prato = nomeExibicao;
     precoPrato = precoItem;
-
+    FecharPedido();
+    //removerCheck()
 }
 function removerBorda2(classeItem, nomeExibicao, precoItem2) {
     precoItem = precoBebida;
@@ -37,6 +48,7 @@ function removerBorda2(classeItem, nomeExibicao, precoItem2) {
     botao.classList.add("borda");
     bebidaEscolhida = nomeExibicao;
     precoBebida = precoItem2;
+    FecharPedido();
 }
 function removerBorda3(classeItem, nomeExibicao, precoItem3) {
     precoItem = precoSobremesa;
@@ -49,15 +61,15 @@ function removerBorda3(classeItem, nomeExibicao, precoItem3) {
     sobremesaEscolhida = nomeExibicao;
     precoSobremesa = precoItem3;
     FecharPedido();
-    // total = precoPrato + precoBebida + precoSobremesa;
 }
-
-
-
 function finalizarPedido() {
-    FecharPedido();
-    let mensagem = null;
-    mensagem = 'Olá, gostaria de fazer o pedido:\n- Prato: ' + prato + '\n- Bebida: ' + bebidaEscolhida + '\n- Sobremesa: '+ sobremesaEscolhida + '\nTotal: R$ ' + total;
-    window.open(`https://wa.me/9999999999999?text=${encodeURIComponent(mensagem)}`)
+    if (prato !== null && bebidaEscolhida !== null && sobremesaEscolhida !== null) {
+        FecharPedido();
+        total = (precoPrato + precoBebida + precoSobremesa);
+        let mensagem = null;
+        mensagem = `Olá, gostaria de fazer o pedido: \n- Prato: ${prato} \n- Bebida: ${bebidaEscolhida} \n- Sobremesa: ${sobremesaEscolhida} \nTotal: R$ ${total.toFixed(2)}`;
+        window.open(`https://wa.me/9999999999999?text=${encodeURIComponent(mensagem)}`)
+    } else {
+        return;
+    }
 }
-
